0f88d7d5dccbf3fb454ab042d2aeb895f30d9407
moldovanjason <moldovanjason@gmail.com>
2020-07-22 13:37:26 +0000

    first commit

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,20 @@
-// Your code here
+let liList = ["Eat", "Drink", "Sleep"];
+let listToShow = liList.map(function(li) {
+	return '<li><span><i class="fa fa-trash"></i></span>' + li + "</li>";
+});
+function keyupfunction(event) {
+	if (event.KeyCode === 13) {
+		liList.push(document.querySelector("#addToDo").value);
+		listToShow = liList.map(function(li) {
+			return '<li><span><i class="fa fa-trash"></i></span>' + li + "</li>";
+		});
+	}
+}
+
+console.log(listToShow);
+document.querySelector("#ulElem").innerHTML = listToShow.join(" ");
+document.querySelector("#addToDo").addEventListener("keyup", keyupfunction);
+
+// liList.map(function(li) {
+// 	document.querySelector("#ulElem").innerHTML = li;
+// });
